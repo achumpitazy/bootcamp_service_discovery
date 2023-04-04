c34b39c268579fa9acb74bdedd656e84ee560dcf
Alan Rufino Chumpitaz Yaya <achumpiy@emeal.nttdata.com>
2023-04-04 12:31:58 -0500

    eureka

---
+++ src/main/java/com/bootcamp/discovery/BootcampServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.bootcamp.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BootcampServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampServiceDiscoveryApplication.class, args);
+	}
+
+}
